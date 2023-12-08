05aa7081e8e7613e929a11c4f287e91fe7cce128
rahul72925 <rahulkhatri72925@gmail.com>
2023-12-08 19:31:19 +0530

    initial commit

---
+++ do-select-react/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <input placeholder="enter you name" name="username" type="text" />
+    </div>
+  );
+}
+
+export default App;
